52b0d69ceeefcc135e942e83ec1d919973e06667
cagin <nebu.gomm@gmail.com>
2020-08-25 00:00:00 +0200

    added sample scene and sample adventurer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import './App.css';
+import * as PIXI from 'pixi.js';
+import adventurerImg from './adventurer.png';
+
+function initWhateverse(wrapper) {
+  const app = new PIXI.Application({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundColor: 0x000000,
+    resolution: window.devicePixelRatio || 1
+  });
+
+  const container = new PIXI.Container();
+  app.stage.addChild(container);
+
+
+  const texture = PIXI.Texture.from(adventurerImg);
+  const adventurer = new PIXI.Sprite(texture);
+  adventurer.anchor.set(0.5);
+  adventurer.x = app.renderer.width / 2;
+  adventurer.y = app.renderer.height / 2;
+  container.addChild(adventurer);
+
+  wrapper.appendChild(app.view);
+
+}
+
 
 function App() {
-  return (
-    <div className="App">
+  const wrapper = useRef(null);
 
-    </div>
+  useEffect(_ => {
+    if (wrapper) {
+      initWhateverse(wrapper.current);
+    }
+  }, [wrapper]);
+
+  return (
+    <div {...{ ref: wrapper }} />
   );
 }
 
